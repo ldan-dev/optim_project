67052310182b786ddad5b08062fd8ccdfb909b52
Caleb de Jesús Raya Mendoza <63015877+Caleto09@users.noreply.github.com>
2026-02-17 16:56:02 -0600

    Add plot2d method to Func_Sphere class
    Refactor Func_Sphere to include plot2d method for visualization.

--- code/func_sphere.py
+++ code/func_sphere.py
@@ -13,31 +13,34 @@ from function import Function
 from plot import Plot
 
 class Func_Sphere(Function):
-    """
-    Docstring for Func_Sphere
-    """
     def __init__(self, limits=[-5, 5]):
         self.limits = limits
-        self.path = []
+        self.path = [] 
+
     def eval(self, x: np.ndarray) -> float:
         return np.sum(x**2)
+
     def diff(self, x: np.ndarray) -> np.ndarray:
         return 2 * np.array(x)
+
     def ddiff(self, x: np.ndarray) -> np.ndarray:
-        return 2 * np.eye(len(x)) #matriz identidad de len(x)
+        return 2 * np.eye(len(x))
+
+    def plot2d(self, path=None):
+        if path is not None:
+            self.path = path
+
+        canvas1 = Plot(title="Gráfica de la Esfera")
+        canvas1.canvas()
+        
+        canvas1.draw_contours(function_obj=self, range_val=self.limits)
+        
+        canvas1.draw_trace(path_points=self.path)
+        canvas1.show()
 
 def main():
-    """  Docstring for main  """
-    canvas1 = Plot(title="Gráfica de la Esfera")
-    canvas1.canvas()
-
     f1 = Func_Sphere(limits=[-10, 10])
-    f1.path = [[-8, 8], [-4, 4], [-2, 2], [0, 0]]
+    f1.plot2d(path=[[-8, 8], [-4, 4], [-2, 2], [0, 0]])
 
-    canvas1.draw_contours(function_obj=f1, range_val=f1.limits)
-
-    canvas1.draw_trace(path_points=f1.path)
-    canvas1.show()
-        
 if __name__ == "__main__":
     main()
