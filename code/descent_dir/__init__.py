8a1e8626205974e45d5040c8522699703467b6f2
ldan-dev <ld.avinaneri@ugto.mx>
2026-02-25 17:47:18 -0600

    xs02 newton renamed

--- code/descent_dir/__init__.py
+++ code/descent_dir/__init__.py
@@ -11,18 +11,20 @@ Direcciones disponibles:
 """
 
 from .gradient import gradient_dir
-from .hessian import hessian_dir
+# from .hessian import hessian_dir
+from .newton import newton_dir
 
 # Diccionario para selección de direcciones por nombre
 DIRECTIONS = {
     "dg": gradient_dir,      # dg = descent gradient
     "gradient": gradient_dir,
-    "newton": hessian_dir,
-    "hessian": hessian_dir,
+    "newton": newton_dir,
+    "hessian": newton_dir,
 }
 
 __all__ = [
     "gradient_dir",
-    "hessian_dir",
+    # "hessian_dir",
+    "newton_dir",
     "DIRECTIONS",
 ]
